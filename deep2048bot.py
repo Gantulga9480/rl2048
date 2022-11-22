97c5d15699b0f1d91b965da31554fef464663afb
Gantulga <limited.tulgaa@gmail.com>
2022-11-23 01:32:58 +0800

    working deep rl bot

---
+++ deep2048bot.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from py2048 import Py2048
+import numpy as np
+
+
+class Deep2048Bot(Py2048):
+
+    def __init__(self, render=True) -> None:
+        super().__init__(render=render)
+        self.model = tf.keras.models.load_model('deepmodel/model.h5')
+
+    def USR_loop_start(self):
+        state = self.game_board.get().flatten()
+        action_values = self.model.predict(np.expand_dims(state, axis=0))[0]
+        action = self.get_masked_action(action_values)
+        self.step(action)
+
+    def get_masked_action(self, q_vals):
+        a_min = np.min(q_vals) - 1
+        # not include UNDO
+        for i in range(4):
+            try:
+                self.game_board.possible_actions.index(i)
+            except ValueError:
+                q_vals[i] = a_min
+        return np.argmax(q_vals)
+
+
+if __name__ == '__main__':
+    t = Deep2048Bot()
+    t.mainloop()
